fbda48e90e446dd01f9ee30e4e36723408830d66
Geetanshu Mathur <mathur.geetanshu887@gmail.com>
2018-07-28 09:15:26 +0530

    finalizing the operations

--- make_states.py
+++ make_states.py
@@ -3,5 +3,27 @@ A = 4
 B = 3
 final_A = 2
 
-def make_state(x, y):
-    if int(x+y) 
+def fillA(x, y):
+    x = A
+def fillB(x, y):
+    y = B
+def pourBtoA(x, y):
+    if x + y <= A:
+        x = x + y
+        y = 0
+    else:
+        temp = A - x
+        y = y - temp
+        x = A
+def pourAtoB(x, y):
+    if x + y <= B:
+        x = 0
+        y = x + y
+    else:
+        temp = B - y
+        x = x - temp
+        y = A
+def emptyA(x, y):
+    x=0
+def emptyB(x, y):
+    y=0
